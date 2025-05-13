d0aabc5537c2c6f589b57085a97f2dfab88f3f3b
SRASONY <leeseula0123@gmail.com>
2025-05-13 11:01:45 +0900

    initial setting

---
+++ src/test/java/com/modive/analysis/AnalysisserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modive.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
