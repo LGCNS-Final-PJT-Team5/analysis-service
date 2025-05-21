1de0b6715494c31a76a791a1454e925e7204450b
minjooong <kmj13675@gmail.com>
2025-05-21 12:44:30 +0900

    feat: drive dto 추가

---
+++ src/main/java/com/modive/analysis/dto/Drive.java
@@ -0,0 +1,61 @@
+package com.modive.analysis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+public class Drive {
+    // Partition Key
+    private String userId;
+    // Sort Key
+    private String driveId;
+
+    private Instant startTime;
+    private Instant endTime;
+    private int activeDriveDurationSec;
+
+    private List<TimeWithFlag> suddenAccelerations;
+    private List<TimeWithFlag> sharpTurns;
+    private List<SpeedLog> speedLogs;
+    private List<StartEndTime> idlingPeriods;
+    private List<SpeedRate> speedRate;
+    private List<StartEndTime> reactionTimes;
+    private List<Instant> laneDepartures;
+    private List<StartEndTime> followingDistanceEvents;
+    private List<Instant> inactiveMoments;
+
+
+    //<editor-fold desc="# Inner Classes">
+    @Data
+    public static class SpeedLog {
+        private int period;
+        private int maxSpeed;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class StartEndTime {
+        private Instant startTime;
+        private Instant endTime;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SpeedRate {
+        private String tag;
+        private int ratio;
+    }
+
+    @Data
+    public static class TimeWithFlag {
+        private Instant time;
+        private boolean flag;
+    }
+    //</editor-fold>
+}
