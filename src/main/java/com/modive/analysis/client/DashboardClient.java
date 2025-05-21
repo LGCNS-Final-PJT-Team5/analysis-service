14ff32ae1821d05a1a326bebfd1a1df781aae2af
minjooong <kmj13675@gmail.com>
2025-05-21 18:36:49 +0900

    feat(#4): Open Feign 추가

---
+++ src/main/java/com/modive/analysis/client/DashboardClient.java
@@ -0,0 +1,20 @@
+package com.modive.analysis.client;
+
+import com.modive.analysis.dto.Drive;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "dashboard-service", url = "http://dashboard-service:8080")
+public interface DashboardClient {
+
+    @PostMapping("/dashboard/post-drive/{driveId}")
+    ResponseEntity<Void> createPostDriveDashboard(
+            @RequestHeader("X-User-Id") String userId, // TODO: userId 연동
+            @PathVariable String driveId,
+            @RequestBody Drive drive);
+
+}
