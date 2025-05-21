54e38eb1f2debf5022ae8a38db5e161b06eac3d2
SRASONY <leeseula0123@gmail.com>
2025-05-21 14:31:53 +0900

    feat(#3): EventEntity 추가

--- src/main/java/com/modive/analysis/entity/EventEntity.java
+++ src/main/java/com/modive/analysis/entity/EventEntity.java
@@ -1,4 +1,26 @@
 package com.modive.analysis.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "event")
+@Data
 public class EventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventId;
+
+    private int userId;
+    private int driveId;
+    private String type;
+
+    @Column(name = "event_time")
+    private Instant eventTime;
+
+    private Double gnssX;
+    private Double gnssY;
 }
