b49905a0a5351282fe667c271bb02cf9a53288ee
minjooong <kmj13675@gmail.com>
2025-05-21 12:42:55 +0900

    initial commit

---
+++ src/main/java/com/modive/analysis/AnalysisserviceApplication.java
@@ -0,0 +1,13 @@
+package com.modive.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisserviceApplication.class, args);
+	}
+
+}
